161f807c9c8a5aa61737ac37609b4652a07138d8
rustam <rusitama@gmail.com>
2022-07-10 01:11:48 +0500

    Add homework6

---
+++ homework6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число для проверки является ли она четным ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num = number % 2;
+
+if (num == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
