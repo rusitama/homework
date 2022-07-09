c350a743abc888f9992407467a62e68787e7c851
rustam <rusitama@gmail.com>
2022-07-10 00:28:07 +0500

    Formatting text

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -2,11 +2,13 @@
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 > number2){
+if (number1 > number2)
+{
     Console.WriteLine("Наибольшее число " + number1);
     Console.WriteLine("Наименьшее число " + number2);
 }
-else{
+else
+{
     Console.WriteLine("Наибольшее число " + number2);
     Console.WriteLine("Наименьшее число " + number1);
 }
