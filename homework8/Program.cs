6580a1cfa838f9606e9ec8499551bc393e7c641a
rustam <rusitama@gmail.com>
2022-07-10 01:35:03 +0500

    Add homework8

---
+++ homework8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число для вывода всех чётных чисел от 1 до N ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = number / 2;
+
+int num = 2;
+
+while (count > 0)
+{
+    if (num == 2)
+    {
+        Console.Write(num);
+    }
+    else
+    {
+        Console.Write(", " + num);
+    }
+
+    num = num + 2;
+    count--;
+}
