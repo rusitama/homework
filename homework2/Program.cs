de2baf56f86a6ae1720891f7e9a661d633b3d7c5
rustam <rusitama@gmail.com>
2022-07-10 00:23:08 +0500

    Add homework2

---
+++ homework2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2){
+    Console.WriteLine("Наибольшее число " + number1);
+    Console.WriteLine("Наименьшее число " + number2);
+}
+else{
+    Console.WriteLine("Наибольшее число " + number2);
+    Console.WriteLine("Наименьшее число " + number1);
+}
