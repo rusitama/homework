7bb5408a760de8cdb888d8ac6a5626180b77b4c8
rustam <rusitama@gmail.com>
2022-07-19 14:15:52 +0500

    Add homework 10

---
+++ homework10/Program.cs
@@ -0,0 +1,26 @@
+﻿string Number(string number){
+    if (number.Length == 3){
+        number = number[1].ToString();
+    }
+    else{
+        Console.WriteLine("Вы ввели не 3х значное число! Повторите попытку!");
+        number = "";
+        getConsole(number);
+    }
+    return number;
+}
+
+void getConsole(string number){
+    //Console.ReadLine();
+    if (number == ""){
+       number = Console.ReadLine(); 
+       Console.WriteLine(Number(number));
+    }
+    else{
+        Console.WriteLine(Number(number));
+    }
+}
+
+Console.WriteLine("Vvedite 3x znachnoe chislo ");
+string number = Console.ReadLine();
+getConsole(number);
