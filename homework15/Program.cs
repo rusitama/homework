2b0b134da5a4d5a0e745714385ab36d6fd6be9ca
rustam <rusitama@gmail.com>
2022-07-19 15:31:50 +0500

    Add homework 13

---
+++ homework15/Program.cs
@@ -0,0 +1,15 @@
+﻿string Number(string number){
+    string otvet = "";
+    int num = Convert.ToInt32(number);
+    if (num == 6 | num == 7){
+        otvet = "да";
+    }
+    else{
+       otvet = "нет";
+    }
+    return otvet;
+}
+
+Console.WriteLine("Введите день недели: ");
+string number = Console.ReadLine();
+ Console.WriteLine(Number(number));
