a5f4331b4ea5d006d53e4c5ac194005f43a6f345
rustam <rusitama@gmail.com>
2022-07-19 14:58:43 +0500

    Add homework 13

---
+++ homework13/Program.cs
@@ -0,0 +1,14 @@
+﻿string Number(string number){
+    if (number.Length >= 3){
+        number = number[2].ToString();
+    }
+    else{
+       number = "третьей цифры нет";
+    }
+    return number;
+}
+
+Console.WriteLine("Введите 3х значное число: ");
+string number = Console.ReadLine();
+ Console.WriteLine(Number(number));
+
