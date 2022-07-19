0826c36a3e4421a911bbadad2ac0ae3189ea8133
rustam <rusitama@gmail.com>
2022-07-19 22:02:27 +0500

    Add homework 23

---
+++ homework23/Program.cs
@@ -0,0 +1,29 @@
+﻿void getConsole(string number)
+{
+    int count = Convert.ToInt32(number);
+
+    int num = 1;
+
+    while (count > 0)
+    {
+        if (num == 1)
+        {
+            Console.Write(num * num * num);
+        }
+        else
+        {
+            Console.Write(", " + num * num * num);
+        }
+
+        num = num + 1;
+        count--;
+    }
+}
+
+Console.WriteLine("Введите число для вывода которая выдаёт таблицу кубов чисел от 1 до N ");
+string number = Console.ReadLine();
+getConsole(number);
+
+
+
+
