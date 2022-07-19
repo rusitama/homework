5848fea10155860a471c504071f1fd7d1d10fca5
rustam <rusitama@gmail.com>
2022-07-19 18:39:35 +0500

    Add homework 19

---
+++ homework19/Program.cs
@@ -0,0 +1,20 @@
+﻿string Number(string number){
+    string a1 = "", a2 = "", a3 = "", a4 = "";
+    string otvet = "нет";
+    bool x1 = false;
+    if (number.Length == 5){
+        a1 = number[0].ToString();
+        a2 = number[1].ToString();
+        a3 = number[3].ToString();
+        a4 = number[4].ToString();
+
+        if (a1 == a4 & a2 == a3){
+            otvet = "да";
+        }        
+    }
+    return otvet;
+}
+
+Console.WriteLine("Введите 5ти значное число для проверки на полиндромность числа: ");
+string number = Console.ReadLine();
+ Console.WriteLine(Number(number));
