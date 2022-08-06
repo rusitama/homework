205ccbe74e1a9865d52cf28a7e356f6d5c321578
rustam <rusitama@gmail.com>
2022-08-06 23:49:12 +0500

    Add homework 27

---
+++ homework27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число для вывода сумму чисел");
+string number = Console.ReadLine();
+Console.WriteLine(Number(number));
+
+int Number(string number){
+    int count = number.Length;
+    int sum = 0, index = 0;
+    while (count > 0){
+        sum = sum + Convert.ToInt32(number[index].ToString());
+        count--;
+        index++;
+    }
+    return sum;
+}
