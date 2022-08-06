2d115d43a71ab0a1f3c90e272865e7ba18852528
rustam <rusitama@gmail.com>
2022-08-06 19:51:35 +0500

    Add homework 24

---
+++ homework24/Program.cs
@@ -0,0 +1,35 @@
+﻿int Number(string number){
+    number = number.Replace(" ", string.Empty);
+    int A = 0, B = 0, C = 0;
+    int count = number.Length;
+    int index = 0;
+    string str = "";
+    while (count > 0){
+        if (number[index].ToString() == ","){
+            A = Convert.ToInt32(str);
+            str = "";
+        }
+        else{
+            str = str + number[index].ToString();
+        }
+        count--;
+        index++;
+        if (count == 0){
+            B = Convert.ToInt32(str);
+        }
+    }
+    while (B > 0){
+        if (C == 0){
+            C = A * A;
+        }
+        else{
+            C = C * A;
+        }
+        
+        B--; 
+    }
+    return C;
+}
+Console.WriteLine("Введите два числа (А и В) для возведения в А степень В");
+string number = Console.ReadLine();
+Console.WriteLine(Number(number));
