38a6d3ef46eb10431dbbbc193d2c4eef29a4db7d
rustam <rusitama@gmail.com>
2022-08-07 15:35:54 +0500

    Add homework 34

---
+++ homework34/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] myArray(){
+    int[] myArray = new int[4];
+    System.Random random = new System.Random();
+    for(int i = 1; i < 5; i++){
+        myArray[i - 1] = random.Next(100, 999);
+    }
+    return myArray;
+}
+
+int[] myNumbers = myArray();
+int i = myNumbers.Length;
+int index = 0, countNum = 0;
+while (i > 0){
+    if ((myNumbers[index] % 2) == 0){
+        countNum++;
+    }
+    i--;
+    index++;
+}
+var strArray = string.Join(", ", myNumbers);
+Console.WriteLine("[{0}] -> " + countNum, strArray);
