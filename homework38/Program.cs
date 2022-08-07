d6a4ae62cb620ba9f28a1fd82a01cdb18f3aca3d
rustam <rusitama@gmail.com>
2022-08-07 21:55:01 +0500

    Add homework 38

---
+++ homework38/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] myArray(){
+    int[] myArray = new int[4];
+    System.Random random = new System.Random();
+    for(int i = 1; i < 5; i++){
+        myArray[i - 1] = random.Next(-100, 100);
+    }
+    return myArray;
+}
+
+int[] myNumbers = myArray();
+int max = myNumbers[0], min = myNumbers[1];
+for (int i = 0; i < 4; i++){
+    if(myNumbers[i] > max){
+        max = myNumbers[i];
+    }
+    if (myNumbers[i] < min){
+        min = myNumbers[i];
+    }
+}
+
+var strArray = string.Join(", ", myNumbers);
+Console.WriteLine("[{0}] -> max " + max + " min " + min + " разница " + (max - min), strArray);
