5116ab4b69c6b80ce92054809a79c64ffbe84354
rustam <rusitama@gmail.com>
2022-08-27 17:31:06 +0500

    Add homework 64

---
+++ homework64/Program.cs
@@ -0,0 +1,17 @@
+﻿void PrintNumber(int n, int m){
+    string result = string.Empty;
+    for (int i = n; i <= m; i++){
+        if (i == m)
+        result += $"{i}";
+        else
+        result += $"{i}, ";
+    }    
+    Console.WriteLine(result);
+}
+
+Console.WriteLine("Введите два натуральных числа для вывода всех чисел в промежутке");
+Console.WriteLine("Первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+PrintNumber(n, m);
