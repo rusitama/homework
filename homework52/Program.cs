abb2246de518ed0f3ce1fd880a18793da67da540
rustam <rusitama@gmail.com>
2022-08-27 13:37:31 +0500

    Add homework 52

---
+++ homework52/Program.cs
@@ -0,0 +1,40 @@
+﻿void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr){
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+           matr[i, j] = new Random().Next(1, 10); 
+        }
+    }
+}
+
+void GetAvarage(int[,] matr){
+    string mass = "Среднее арифметическое каждого столбца: ";
+    for (int i = 0; i < matr.GetLength(0); i++){
+        double sum = 0;
+        for (int j = 0; j < matr.GetLength(1); j++){
+            sum = sum + Convert.ToDouble(matr[i, j]);
+        }
+        mass = mass + (sum / matr.GetLength(1)) + "; ";            
+    }
+    Console.WriteLine(mass);
+}
+
+
+int[,] matrix = new int[3, 4]; //объявляем массив
+
+FillArray(matrix);
+PrintMatrix(matrix);
+GetAvarage(matrix);
