4704b7aef076e6a3706a7d8c515b3b38a891b47b
rustam <rusitama@gmail.com>
2022-08-27 18:51:32 +0500

    Add homework 68

---
+++ homework68/Program.cs
@@ -0,0 +1,11 @@
+﻿int Akk(int m, int n){
+     if (m == 0) return (n + 1);
+     if (m != 0 && n == 0) return Akk(m - 1, 1);
+     return Akk(m - 1, Akk(m, n - 1));
+}
+
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Akk(m, n));
