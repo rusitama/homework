b6689e912a75f15cbf43ea48e50be157cba8f80c
rustam <rusitama@gmail.com>
2022-08-27 17:53:58 +0500

    Add homework 66

---
+++ homework66/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintNumber(int n, int m){
+    int sum = 0;
+    for (int i = n; i <= m; i++){
+        sum += i;
+    }    
+    Console.WriteLine(sum);
+}
+
+Console.WriteLine("Введите два натуральных числа для вывода суммы всех чисел в промежутке");
+Console.WriteLine("Первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+PrintNumber(n, m);
