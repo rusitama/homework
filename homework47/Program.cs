338a31265df68aa0cd8aac60e0ad4b04eb5920c8
rustam <rusitama@gmail.com>
2022-08-27 11:59:17 +0500

    Add homework 47

---
+++ homework47/Program.cs
@@ -0,0 +1,34 @@
+﻿void PrintMatrix(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr){
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+           matr[i, j] = Math.Round((new Random().NextDouble()) * (new Random().Next(-10, 10)), 2); 
+        }
+    }
+}
+
+Console.WriteLine("Введите чила для вывода двумерного массива");
+Console.WriteLine("Введите длину колонок");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число строк");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n]; //объявляем массив
+
+PrintMatrix(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
