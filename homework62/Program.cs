181daf0e689085c6e05d35050827f5cdfde9bd2b
rustam <rusitama@gmail.com>
2022-08-27 16:31:19 +0500

    Add homework 62

---
+++ homework62/Program.cs
@@ -0,0 +1,43 @@
+﻿const int n = 4;
+const int m = 4;
+int[,] matrix = new int[n, m];
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    int row = 0;
+    int col = 0;
+    int dx = 1;
+    int dy = 0;
+    int dirChanges = 0;
+    int visits = m;
+
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        matrix[row, col] = i + 1;
+        if (--visits == 0)
+        {
+            visits = m * (dirChanges % 2) + n * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+            int temp = dx;
+            dx = -dy;
+            dy = temp;
+            dirChanges++;
+        }
+        col += dx;
+        row += dy;
+    }
+}
+
+FillArray(matrix);
+PrintMatrix(matrix);
